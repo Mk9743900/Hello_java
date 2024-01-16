ffc29441b9cf1bbc5ba64e0d3aa15fc0e64d5c9b
Mk9743900 <143392275+Mk9743900@users.noreply.github.com>
2024-01-16 04:58:10 +0000

    java done

--- forJava/01_java/Main.java
+++ forJava/01_java/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
+    public static int add(int num){
+        int result = num;
+        return result;
+    }
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println(add(10));
+        
     }
 }
